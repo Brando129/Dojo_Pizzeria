b3bb0a7472aa202ed2304233405c0be8749a69a2
Brandon Colangelo <brandoncancode@gmail.com>
2023-03-30 09:25:44 -0400

    Update pizzeria.js
    Added some documentation.

--- Dojo_Pizzeria/pizzeria.js
+++ Dojo_Pizzeria/pizzeria.js
@@ -1,14 +1,18 @@
 // Create a function that makes a random Pizza
+
+// Ingrdeient options
 var crusts = ["traditional", "hand tossed", "deep dish", "thin crust", "stuffed crust", "gluten free"]
 var sauces = ["marinara", "white sauce", "traditional", "three cheese", "BBQ"]
 var cheeses = ["mozzarella", "provolone", "cheddar", "sharp", "whistle pig", "colby jack", "parmesean"]
 var toppings = ["sausage", "pepperoni", "ham", "onions", "mushrooms", "peppers", "bacon", "pineapples", "olives", "chicken"]
 
+// Function for randomly picking 
 function randomPick(arr){
     var i = Math.floor(arr.length * Math.random());
     return arr[i];
 }
 
+// Function that builds the random pizza
 function randomPizza(){
     var customPizza = {};
     customPizza.crusts = randomPick(crusts);
